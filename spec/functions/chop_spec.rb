e4173cb2d6347059c03afa22eed7e4a6ee1766ec
Paula McMaw <paula@puppet.com>
2017-02-21 14:47:26 +0000

    Adding unit test for i18n chop function

--- spec/functions/chop_spec.rb
+++ spec/functions/chop_spec.rb
@@ -17,4 +17,8 @@ describe 'chop' do
   it { is_expected.to run.with_params(AlsoString.new("one\n")).and_return("one") }
   it { is_expected.to run.with_params(AlsoString.new("one\n\n")).and_return("one\n") }
   it { is_expected.to run.with_params([AlsoString.new("one\n"), AlsoString.new("two"), "three\n"]).and_return(["one", "tw", "three"]) }
+
+  it { is_expected.to run.with_params("新しい分野").and_return("新しい分") }
+  it { is_expected.to run.with_params("新しい分野\n\n").and_return("新しい分野\n") }
+  it { is_expected.to run.with_params(["新し\n", "しい", "い分\n"]).and_return(["新し", "し", "い分"]) }
 end
