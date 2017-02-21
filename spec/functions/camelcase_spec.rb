e87d26e4a0cb3042e8c8872a7c0ac49647e5d566
Paula McMaw <paula@puppet.com>
2017-02-21 14:48:15 +0000

    Adding unit test for i18n camelcase function

--- spec/functions/camelcase_spec.rb
+++ spec/functions/camelcase_spec.rb
@@ -14,4 +14,9 @@ describe 'camelcase' do
   it { is_expected.to run.with_params([]).and_return([]) }
   it { is_expected.to run.with_params(["abc", "aa_bb_cc"]).and_return(["Abc", "AaBbCc"]) }
   it { is_expected.to run.with_params(["abc", 1, "aa_bb_cc"]).and_return(["Abc", 1, "AaBbCc"]) }
+
+  it { is_expected.to run.with_params("åъċ").and_return("Åъċ") }
+  it { is_expected.to run.with_params("ååææçç").and_return("ÅåÆæÇç") }
+  it { is_expected.to run.with_params(["åъċ", "åå_ææ_ċċ"]).and_return(["Åъċ", "ÅåÆæÇç"]) }
+  it { is_expected.to run.with_params(["åæç", 1, "åå_ææ_çç"]).and_return(["Åæċ", 1, "ÅåÆæÇċ"]) }
 end
