c0df74e0d90b5da4f08e723d48f9e0062f52a7c0
Paula McMaw <paula@puppet.com>
2017-02-21 14:48:38 +0000

    Adding unit test for i18n capitalize function

--- spec/functions/capitalize_spec.rb
+++ spec/functions/capitalize_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+# Editor to capitalize srings including UTF8: http://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_touppercase
+
 describe 'capitalize' do
   it { is_expected.not_to eq(nil) }
   it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError) }
@@ -12,4 +14,21 @@ describe 'capitalize' do
   it { is_expected.to run.with_params([]).and_return([]) }
   it { is_expected.to run.with_params(["one", "two"]).and_return(["One", "Two"]) }
   it { is_expected.to run.with_params(["one", 1, "two"]).and_return(["One", 1, "Two"]) }
+  
+  # Extended ASCII characters
+  it { is_expected.to run.with_params("å").and_return("Å") }
+  it { is_expected.to run.with_params("æ").and_return("Æ") }
+  it { is_expected.to run.with_params("ç").and_return("Ç") }
+
+  # Vowels with diaresis
+  it { is_expected.to run.with_params("á").and_return("Á") }
+  it { is_expected.to run.with_params("ë").and_return("Ë") }
+  it { is_expected.to run.with_params("ï").and_return("Ï") }
+  it { is_expected.to run.with_params("ố").and_return("Ố") }
+  it { is_expected.to run.with_params("ü").and_return("Ü") }
+  it { is_expected.to run.with_params("ü").and_return("Ü") }
+  it { is_expected.to run.with_params("theüthing").and_return("Theüthing") }
+
+  # UTF8 String - https://www.tienhuis.nl/utf8-generator
+  it { is_expected.to run.with_params("čǻρίţāŀіşè₣ụאָčţìŏאּ").and_return("Čǻρίţāŀіşè₣ụאָčţìŏאּ") }
 end
